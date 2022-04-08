33bd60acc9d38e57b93f8d29b9215bafcd661724
AndresGarciaGo <vi01849@innovaccion.mx>
2022-04-08 01:29:27 -0500

    prueba 1

--- src/screens/consts/restaurants.js
+++ src/screens/consts/restaurants.js
@@ -1,36 +1,36 @@
-const hotels = [
+const restaurants = [
   {
     id: '1',
-    name: 'Hotel Malinalco',
+    name: 'Restaurante Malinalco',
     location: 'a 10 minutos del centro',
-    price: 2500,
-    image: require('../assets/hotel1.jpg'),
+    price: 1500,
+    image: require('../assets/rst1.jpg'),
     details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
   },
   {
     id: '2',
-    name: 'Hotel Malinalquito',
+    name: 'Restaurant Malinalquito',
     location: '20 minutos del centro',
-    price: 2000,
-    image: require('../assets/hotel2.jpg'),
+    price: 1000,
+    image: require('../assets/rst2.jpg'),
     details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
   },
   {
     id: '3',
-    name: 'Hotel Malinalcote',
+    name: 'Restaurant Malinalcote',
     location: '5 minutos del centro',
     price: 1500,
-    image: require('../assets/hotel3.jpg'),
+    image: require('../assets/rst3.jpg'),
     details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
   },
   {
     id: '4',
-    name: 'Hotel Andres',
+    name: 'Restaurant Andres',
     location: '15 min del centro',
-    price: 50000,
-    image: require('../assets/hotel4.jpg'),
+    price: 3000,
+    image: require('../assets/rst4.jpg'),
     details: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id leo. Velit euismod in pellentesque massa placerat duis ultricies lacus sed. Justo laoreet sit amet cursus sit`,
   },
 ];
 
-export default hotels;
+export default restaurants;
