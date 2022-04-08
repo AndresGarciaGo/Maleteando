9c306249c8456256d4a185ec066d9c67d1519cc9
MontyM31 <ya.montoya.m@edu.utc.mx>
2022-04-08 17:44:35 -0500

    rep1.2

--- src/screens/Menus.js
+++ src/screens/Menus.js
@@ -45,7 +45,7 @@ export const Menus = ({ navigation }: Props) => {
       </TouchableOpacity>
 
       <TouchableOpacity style={styles.cont}
-        onPress={() => navigation.navigate('Historia')}>
+        onPress={() => navigation.navigate('Home2')}>
           <Image source={hotel} style={styles.img}/>
           <Text  style={styles.te}>GASTRONOMIA</Text>
       </TouchableOpacity>
