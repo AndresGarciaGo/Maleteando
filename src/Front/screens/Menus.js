8a6bb7523fa7cef1b036c7c8d69762d725492536
MontyM31 <ya.montoya.m@edu.utc.mx>
2022-06-10 00:56:31 -0500

    a

--- src/Front/screens/Menus.js
+++ src/Front/screens/Menus.js
@@ -8,7 +8,7 @@ import hotel from './assets/hotel1.jpg';
 import hotel2 from './assets/hotel2.jpg';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import COLORS from './consts/colors';
-import { FlatList, ScrollView } from 'react-native-gesture-handler';
+import { ScrollView } from 'react-native-gesture-handler';
 import { SlideFromRightIOS } from '@react-navigation/stack/lib/typescript/src/TransitionConfigs/TransitionPresets';
 import { render } from 'react-native/Libraries/Renderer/implementations/ReactNativeRenderer-prod';
 import CasaC from './assets/CasaC.jpg'
@@ -16,118 +16,110 @@ import ZonaA from './assets/ZonaA.jpg';
 import Gastro from './assets/Gastro.jpg';
 import Paq from './assets/Paq.jpg';
 
+interface Props extends StackScreenProps<any, any> { };
 
-
-const boton = [
-  {
-    id: 'Historia',
-    name: 'HISTORIA',
-    image: require('./assets/CasaC.jpg'),
-  },
-
-  {
-    id: 'AtractivosTuristicos',
-    name: 'ATRACTIVOS',
-    image: require('./assets/ZonaA.jpg'),  
-  },
-
-  {
-    id: 'Restaurantes',
-    name: 'GASTRONOMIA',
-    image: require('./assets/Gastro.jpg'),  
-  },
-
-  {
-    id: 'Hoteles',
-    name: 'HOSPEDAJE',
-    image: require('./assets/hotel1.jpg'),  
-  },
-  {
-    id: 'Paquetes',
-    name: 'PAQUETES',
-    image: require('./assets/Paq.jpg'),  
-  },
-]
-
-export const Menus = ({ navigation, item }) => {
-
-  
-  
-  
+export const Menus = ({ navigation }: Props) => {
   return (
-    
-    <View style={[styles.container, styles.p]}>
+<View style={[styles.container,styles.p]}>
 
-      <Text style={styles.subtitle}> MALINALCO </Text>
+        <Text style={styles.subtitle}> MALINALCO </Text>
       <Text style={styles.subtitlee}> ESTADO DE MÉXICO </Text>
-
+       
       <View style={styles.searchInputContainer}>
-        <Icon name="search" size={35} style={{ marginLeft: 20 }} />
-        <TextInput
-          placeholder="Buscar"
-          style={{ fontSize: 20, paddingRight: 150 }}
-          
-        />
-      </View>
+          <Icon name="search" size={35} style={{ marginLeft: 20 }} />
+          <TextInput
+            placeholder="Buscar"
+            style={{ fontSize: 20, paddingRight: 150 }}
+          />
+        </View>
 
       
+        <ScrollView
+        showsVerticalScrollIndicator={false}>
+
+      <TouchableOpacity style={styles.cont}
+        onPress={() => navigation.navigate('Historia')}>
+        <Image source={CasaC} style={styles.img}/>
+          <Text  style={styles.tex}>HISTORIA</Text>
+      </TouchableOpacity>
         
-          <FlatList
-          data={ boton }
-          
-          keyExtractor={(item, index) => item.url}
-          renderItem = {({item}) => 
-          (<TouchableOpacity style={styles.cont}
-          onPress={() => navigation.navigate(item.id)}>
-          <Image source={item.image} style={styles.img} />
-          <Text style={styles.tex}>{item.name}</Text>
-          </TouchableOpacity>)}
-          />     
-       
-    </View>
+
+      <TouchableOpacity style={styles.cont}
+        onPress={() => navigation.navigate('AtractivosTuristicos')}>
+        <Image source={ZonaA} style={styles.img}/>
+          <Text  style={styles.tex}>ATRACTIVOS</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.cont}
+        onPress={() => navigation.navigate('Restaurantes')}>
+          <Image source={Gastro} style={styles.img}/>
+          <Text  style={styles.tex}>GASTRONOMIA</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.cont}
+        onPress={() => navigation.navigate('Hoteles')}>
+          <Image source={hotel} style={styles.img}/>
+          <Text  style={styles.tex}>HOSPEDAJE</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.cont1}
+        onPress={() => navigation.navigate('Paquetes')}>
+          <Image source={Paq} style={styles.img}/>
+          <Text  style={styles.tex}>PAQUETES</Text>
+      </TouchableOpacity>
+      
+    </ScrollView>
+
+</View>
   )
 }
 
 const styles = StyleSheet.create({
 
+  p:{
     flex:1,
     backgroundColor: 'white',
   },
+
+subtitle: {
+    marginTop:15,
+    fontSize: 32,
+    color: '#2B3030',
     textAlign: 'center',
     fontWeight: 'bold',
-    justifyContent: "space-between",
+    justifyContent:"space-between",
     flexDirection: "row",
 
-  },
-  subtitlee: {
+},
+subtitlee: {
     fontSize: 19,
     color: '#2B3030',
     textAlign: 'center',
     fontWeight: 'bold',
-    justifyContent: "space-between",
+    justifyContent:"space-between",
     flexDirection: "row",
 
-  },
+},
   img: {
-    width: 170,
-    height: 100,
-    borderRadius: 20,
+    width: 170, 
+    height: 100, 
+    borderRadius: 20,  
     marginLeft: 4,
-    marginTop: 4,
+    marginTop: 4, 
 
-  },
+},
 
-  tex: {
+  tex:{
     height: 63,
     width: 180,
     marginLeft: 190,
     fontWeight: 'bold',
     fontSize: 20,
-    alignContent: "center",
+    alignContent:"center",
     marginTop: -59,
   },
 
-
+  
   cont: {
     width: "90%",
     borderWidth: 1,
@@ -136,7 +128,7 @@ const styles = StyleSheet.create({
     borderColor: "#818181",
     boxShadow: '0px 0px 3.5px 0px rgba(0,0,0,0.2)',
     marginTop: 10,
-    alignContent: "center",
+    alignContent:"center",
     marginLeft: 25,
   },
   cont1: {
@@ -147,7 +139,7 @@ const styles = StyleSheet.create({
     borderColor: "#818181",
     boxShadow: '0px 0px 3.5px 0px rgba(0,0,0,0.2)',
     marginTop: 10,
-    alignContent: "center",
+    alignContent:"center",
     marginLeft: 25,
     marginBottom: 10,
   },
@@ -161,7 +153,7 @@ const styles = StyleSheet.create({
 
   searchInputContainer: {
     height: 50,
-    width: "88%",
+    width:"88%",
     backgroundColor: COLORS.light,
     marginTop: 15,
     borderRadius: 30,
